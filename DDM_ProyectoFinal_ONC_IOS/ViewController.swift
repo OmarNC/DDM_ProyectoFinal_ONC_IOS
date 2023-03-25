c82e07f1e8761b7b8c8b13d62341a7085a05c578
Omar NC <omar.nieto@gmail.com>
2023-03-25 00:42:04 -0600

    Initial Commit

---
+++ DDM_ProyectoFinal_ONC_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DDM_ProyectoFinal_ONC_IOS
+//
+//  Created by Omar Nieto on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
